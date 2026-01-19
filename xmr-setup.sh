937227418b36e4da0935c6d6626485364efd1f5c
Arnoll Studio <40510737+theArnoll@users.noreply.github.com>
2026-01-19 11:36:09 +0800

    Fix chkip.sh generating

--- xmr-setup.sh
+++ xmr-setup.sh
@@ -181,7 +181,7 @@ cat lastword.txt
 cat <<EOF > chkip.sh
 IP_ADDR=\$(ip route get 8.8.8.8 | awk '{for(i=1;i<=NF;i++) if (\$i=="src") print \$(i+1)}')
 echo "Current IP Address: \$IP_ADDR"
-echo "So you can connect cockpit at: https://$IP_ADDR:9090"
+echo "So you can connect cockpit at: https://\$IP_ADDR:9090"
 EOF
 chmod +x chkip.sh
 echo The message has been saved to lastword.txt. You can check it again by running \"cat lastword.txt\".
