edcd36b053e11f864b6eac570f1aaaef0c48b3e5
Arnoll Studio <40510737+theArnoll@users.noreply.github.com>
2026-01-19 11:32:26 +0800

    Fix chkip.sh generating

--- xmr-setup.sh
+++ xmr-setup.sh
@@ -179,7 +179,7 @@ You can paste your [coin]:[address] by editing the execution script located at $
 EOF
 cat lastword.txt
 cat <<EOF > chkip.sh
-IP_ADDR=\$(ip route get 8.8.8.8 | awk '{for(i=1;i<=NF;i++) if ($i=="src") print $(i+1)}')
+IP_ADDR=\$(ip route get 8.8.8.8 | awk '{for(i=1;i<=NF;i++) if (\$i=="src") print \$(i+1)}')
 echo "Current IP Address: \$IP_ADDR"
 echo "So you can connect cockpit at: https://$IP_ADDR:9090"
 EOF
